9d5dbccde730730b2fbea63c3af574958aeaa5a8
DanDaMan23 <danlawrcons.gmail.com>
2023-11-23 11:38:59 -0600

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	:root {
+		--pewter: #a1a9b0;
+		--gonmetalGray: #6c6a61;
+		--ivory: #fbf9fb;
+		--black: #33363a;
+
+		background: var(--gonmetalGray);
+		color: var(--ivory);
+	}
+</style>

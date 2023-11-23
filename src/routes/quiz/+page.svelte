b2553ef42e757664411b8e4acca4536630312d52
DanDaMan23 <danlawrcons.gmail.com>
2023-11-23 13:49:58 -0600

    API call setup

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -1,8 +1,30 @@
 <script>
+	import { onMount } from 'svelte';
+	import { questions } from '../../store';
+
 	const API_ENDPOINT = 'https://opentdb.com/api.php?amount=10';
+
+	onMount(async () => {
+		fetch(API_ENDPOINT)
+			.then((response) => response.json())
+			.then((data) => {
+				questions.set(data?.results);
+			})
+			.catch((error) => {
+				console.log(error);
+				return [];
+			});
+	});
 </script>
 
-<div class="quiz">Quiz Page</div>
+<div class="quiz">
+	Quiz Page
+	<ul>
+		{#each $questions as { question }}
+			<li>{@html question}</li>
+		{/each}
+	</ul>
+</div>
 
 <style>
 </style>
